b714adf04959e15a71b7157278647650ad9ae0bb
Thu Ta Ye Yint <mario.thuta@gmail.com>
2024-12-11 19:04:10 +0630

    testing 001

--- assets/html/databaseconnection.php
+++ assets/html/databaseconnection.php
@@ -2,20 +2,19 @@
 
 // Using PDO  (PHP Data object); 
 
-function connect(){
+function connect()
+{
     $server = "localhost"; // database server name or ip
     $user = "root"; // database user account name
     $password = "mydbserver2025"; // database account passsword
     $databasename = "aus";  // database name
 
-    try{
+    try {
         $conn = new PDO("mysql:host=$server;port=3306;dbname=$databasename", $user, $password);
-        $conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            // echo "connected";
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        // echo "connected";
         return $conn;
-    }catch(PDOException $e){
+    } catch (PDOException $e) {
         echo $e;
     }
 }
-
-?>
